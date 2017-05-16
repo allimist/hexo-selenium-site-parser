4f5341ad2215a310ce6898d33e744779abab09a4
alex <a@a.a>
2017-05-16 14:50:43 +0300

    fix

--- hexo-selenium-site-parser.js
+++ hexo-selenium-site-parser.js
@@ -33,22 +33,19 @@ Parser.prototype.parse = function(sites_config) {
     function parse_site(site_config){
 
 
-        var site_url = site_config['site_url'];
-        var records_links_xpath = site_config['records_links_xpath'];
-        var records_title_xpath = site_config['records_title_xpath'];
-        var records_content_xpath = site_config['records_content_xpath'];
-        var records_img_xpath = site_config['records_img_xpath'];
-        var records_imgs_xpath = site_config['records_imgs_xpath'];
-
-        var records_meta1_xpath = site_config['records_meta1_xpath'];
-
-        var offset_of_movies_to_parse = site_config['offset_of_movies_to_parse'];
-        var num_of_movies_to_parse = site_config['num_of_movies_to_parse'];
-        var records_imgs_to_parse = site_config['records_imgs_to_parse'];
-        var link_to_source_page = site_config['link_to_source_page'];
-        var allow_owerite = site_config['allow_owerite']; //  0 = not rewrite /important not remove
-
-
+        //var site_url = site_config['site_url'];
+        //var records_links_xpath = site_config['records_links_xpath'];
+        //var records_title_xpath = site_config['records_title_xpath'];
+        //var records_content_xpath = site_config['records_content_xpath'];
+        //var records_img_xpath = site_config['records_img_xpath'];
+        //var records_imgs_xpath = site_config['records_imgs_xpath']; 
+        //var records_meta1_xpath = site_config['records_meta1_xpath'];
+        //var offset_of_movies_to_parse = site_config['offset_of_movies_to_parse'];
+        //var num_of_movies_to_parse = site_config['num_of_movies_to_parse'];
+        //var records_imgs_to_parse = site_config['records_imgs_to_parse'];
+        //var link_to_source_page = site_config['link_to_source_page'];
+        //var allow_owerite = site_config['allow_owerite']; //  0 = not rewrite /important not remove
+        //var records_translit_title_letters_to_english = site_config['records_translit_title_letters_to_english'];
 
         var links = [];
         var data = [];
@@ -57,8 +54,8 @@ Parser.prototype.parse = function(sites_config) {
 
             //console.log('data parsed');
             //console.log('data parsed',links);
-            driver.get(site_url); 
-            driver.findElements(By.xpath(records_links_xpath)).then(function(newsElems) {
+            driver.get(site_config['site_url']); 
+            driver.findElements(By.xpath(site_config['records_links_xpath'])).then(function(newsElems) {
             
                 newsElems.forEach(function(newsElem) {
 
@@ -78,7 +75,7 @@ Parser.prototype.parse = function(sites_config) {
 
 
             }).then(function(){
-                    console.log('links parsed',links.length);
+                    console.log('page: ',site_config['site_url'],' links parsed: ',links.length);
                     callback(links);
             });
 
@@ -98,7 +95,7 @@ Parser.prototype.parse = function(sites_config) {
 
                 driver.get(item);
 
-                driver.findElement(By.xpath(records_title_xpath)).then(function(checkElem) {
+                driver.findElement(By.xpath(site_config['records_title_xpath'])).then(function(checkElem) {
                     checkElem.getText().then(function(body_text){
                         //console.log('title',body_text);
                         data[index]['title'] = body_text;
@@ -106,7 +103,7 @@ Parser.prototype.parse = function(sites_config) {
                     });
                 });
 
-                driver.findElement(By.xpath(records_content_xpath)).then(function(checkElem) {
+                driver.findElement(By.xpath(site_config['records_content_xpath'])).then(function(checkElem) {
                     checkElem.getText().then(function(body_text){
                         //console.log('desc',body_text);
                         data[index]['desc'] = body_text;
@@ -114,10 +111,10 @@ Parser.prototype.parse = function(sites_config) {
                     });
                 });
 
-                if(records_img_xpath!=''){
+                if(site_config['records_img_xpath']!=''){
 
                     //parse image only if exist
-                    driver.wait(until.elementLocated(By.xpath(records_img_xpath)), 5000).then(function(checkElem) {
+                    driver.wait(until.elementLocated(By.xpath(site_config['records_img_xpath'])), 5000).then(function(checkElem) {
                         //console.log('----yes');
                         checkElem.getAttribute("src").then(function(body_text){
                             data[index]['img'] = body_text;
@@ -135,9 +132,9 @@ Parser.prototype.parse = function(sites_config) {
                 //driver.wait(until.titleIs('webdriver - Google Search'), 1000);
 
 
-                if(records_meta1_xpath!=''){
+                if(site_config['records_meta1_xpath']!=''){
 
-                    driver.findElement(By.xpath(records_meta1_xpath)).then(function(checkElem) {
+                    driver.findElement(By.xpath(site_config['records_meta1_xpath'])).then(function(checkElem) {
                         checkElem.getText().then(function(body_text){
                             console.log('meta1',body_text);
                             data[index]['meta1'] = body_text;
@@ -147,11 +144,11 @@ Parser.prototype.parse = function(sites_config) {
 
                 }
 
-                if(records_imgs_xpath!=''){
+                if(site_config['records_imgs_xpath']!=''){
 
                     var images = [];
 
-                    driver.findElements(By.xpath(records_imgs_xpath)).then(function(checkElems) {
+                    driver.findElements(By.xpath(site_config['records_imgs_xpath'])).then(function(checkElems) {
                         
                         
 
@@ -173,6 +170,34 @@ Parser.prototype.parse = function(sites_config) {
 
                 }
 
+                
+                if(site_config['categories_xpath']!=''){
+
+                    driver.findElement(By.xpath(site_config['categories_xpath'])).then(function(checkElem) {
+                        checkElem.getText().then(function(body_text){
+                            //console.log('title',body_text);
+                            data[index]['categories'] = body_text;
+                            callback(data[index],index);
+                        });
+                    });
+
+                    
+
+                }
+                if(site_config['tags_xpath']!=''){
+
+                    driver.findElement(By.xpath(site_config['tags_xpath'])).then(function(checkElem) {
+                        checkElem.getText().then(function(body_text){
+                            //console.log('title',body_text);
+                            data[index]['tags'] = body_text;
+                            callback(data[index],index);
+                        });
+                    });
+
+                    
+
+                }
+
         };
 
 
@@ -182,11 +207,15 @@ Parser.prototype.parse = function(sites_config) {
               //console.log('-no title');
             } else if (typeof record['desc'] == 'undefined' || record['desc'] == null){
                //console.log('-no desc');
-            } else if (records_img_xpath != '' && (typeof record['img'] == 'undefined' || record['img'] == null)){
+            } else if (site_config['records_img_xpath'] != '' && (typeof record['img'] == 'undefined' || record['img'] == null)){
                //console.log('-no img');
-            } else if (records_meta1_xpath != '' && (typeof record['meta1'] == 'undefined' || record['meta1'] == null)){
+            } else if (site_config['records_meta1_xpath'] != '' && (typeof record['meta1'] == 'undefined' || record['meta1'] == null)){
                //console.log('-no iframe');
-            } else if (records_imgs_xpath != '' && (typeof record['imgs'] == 'undefined' || record['imgs'] == null)){
+            } else if (site_config['records_imgs_xpath'] != '' && (typeof record['imgs'] == 'undefined' || record['imgs'] == null)){
+               //console.log('-no iframe');
+            } else if (site_config['categories_xpath'] != '' && (typeof record['categories'] == 'undefined' || record['categories'] == null)){
+               //console.log('-no iframe');
+            } else if (site_config['tags_xpath'] != '' && (typeof record['tags'] == 'undefined' || record['tags'] == null)){
                //console.log('-no iframe');
             } else {
                 
@@ -212,17 +241,22 @@ Parser.prototype.parse = function(sites_config) {
                 post_name = post_name.replace(/\]/g, '_');
                 post_name = post_name.replace(/\[/g, '_');
                 post_name = post_name.replace(/\|/g, '_');
-                post_name = post_name.replace(/&/g, 'and');
+                post_name = post_name.replace(/&/g, '_');
                 post_name = post_name.replace(/-/g, '_');
                 post_name = post_name.replace(/__/g, '_');
-                post_name = translit(post_name);
+                //value = value.replace(/(^[\s]+|[\s]+$)/g, '');
+
+                if(site_config['records_translit_title_letters_to_english']){
+                    post_name = translit(post_name);
+                }
+                
 
                 //console.log(post_name);
 
                 //first chatck if exist and if allow owerite
                 
 
-                if (!allow_owerite && fs2.existsSync('source/_posts/'+post_name+'.md')) {
+                if (!site_config['allow_owerite'] && fs2.existsSync('source/_posts/'+post_name+'.md')) {
                     // Do something
                     console.log('- '+index+' file exist - owerite disabled',post_name);
 
@@ -239,12 +273,12 @@ Parser.prototype.parse = function(sites_config) {
                         data = data.replace(/\[\[title\]\]/g, record['title']);
                         data = data.replace('[[desc]]', record['desc']);
 
-                        if(records_meta1_xpath!=''){
+                        if(site_config['records_meta1_xpath']!=''){
                             data = data.replace(/\[\[meta1\]\]/g, record['meta1']);
                         }
 
                         //store image if exist
-                        if(records_img_xpath != '' && record['img']!=''){
+                        if(site_config['records_img_xpath'] != '' && record['img']!=''){
                             data = data.replace('[[img]]', '!['+record['title']+'](/images/'+post_name+'.jpeg "'+record['title']+'")');
 
                             request(record['img']).pipe(fs2.createWriteStream('source/images/'+post_name+'.jpeg'));
@@ -254,7 +288,7 @@ Parser.prototype.parse = function(sites_config) {
 
 
                         //store images ???
-                        if(records_imgs_xpath != '' && record['imgs'].length>0){
+                        if(site_config['records_imgs_xpath'] != '' && record['imgs'].length>0){
 
                                 if (!fs.existsSync('source/images/'+post_name)){
                                     fs.mkdirSync('source/images/'+post_name);
@@ -265,7 +299,7 @@ Parser.prototype.parse = function(sites_config) {
                                 img_index=0;
                                 record['imgs'].forEach(function(checkElem) {
 
-                                    if(img_index>records_imgs_to_parse){
+                                    if(img_index>site_config['records_imgs_to_parse']){
                                         return;
                                     }
 
@@ -284,7 +318,83 @@ Parser.prototype.parse = function(sites_config) {
                             data = data.replace('[[imgs]]', '');
                         }
 
-                        if(link_to_source_page){
+                        if(site_config['categories_xpath'] != '' && record['categories'].length>0){
+
+                            
+                            // console.log(record['tags']);
+                            var records = record['categories'].split(': ');
+                            // console.log(records);
+                            records = records[1];
+                            // console.log(records);
+                            records = records.split(',');
+                            // console.log(records);
+
+                            var records_up = [];
+                            records.forEach(function(value){
+
+                                value = value.replace(/(^[\s]+|[\s]+$)/g, '');
+                                records_up.push('- '+value+'\n');
+
+
+                            });
+                            // console.log(records_up);
+                            records_up = records_up.join('');
+                            // console.log(records_up);
+
+
+
+
+                            data = data.replace('[[categories]]', records_up); 
+
+                            
+                        } else{
+                            data = data.replace('[[categories]]', '');
+                        }
+                        if(site_config['tags_xpath'] != '' && record['tags'].length>0){
+
+                            
+                            // console.log(record['tags']);
+                            var records = record['tags'].split(': ');
+                            // console.log(records);
+                            records = records[1];
+                            // console.log(records);
+                            records = records.split(',');
+                            // console.log(records);
+
+                            var records_up = [];
+                            records.forEach(function(value){
+
+                                value = value.replace(/(^[\s]+|[\s]+$)/g, '');
+                                records_up.push('- '+value+'\n');
+
+
+                            });
+                            // console.log(records_up);
+                            records_up = records_up.join('');
+                            // console.log(records_up);
+
+
+
+
+                            data = data.replace('[[tags]]', records_up); 
+
+                            
+                        } else{
+                            data = data.replace('[[tags]]', '');
+                        }
+
+                        data = data.replace('[[iframe]]', ''); 
+
+
+
+                        
+
+
+
+
+
+
+                        if(site_config['link_to_source_page']){
                             data = data.replace(/\[\[link\]\]/g, record['link']);
                         }
 
@@ -334,11 +444,11 @@ Parser.prototype.parse = function(sites_config) {
               //console.log("each", item, index, arr);
 
                 
-                if(index<offset_of_movies_to_parse){
+                if(index<site_config['offset_of_pages_to_parse']){
                     return;
                 }
 
-                if(index>=num_of_movies_to_parse){
+                if(index>=site_config['num_of_pages_to_parse']){
                     return;
                 }
 
